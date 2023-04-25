323b53b4db3df4f30af66004792c268676b4f130
Halobrobro <Halobrobro@mail.ru>
2023-04-25 16:54:22 +0700

    izmenili Program.cs

--- hello/hello/Program.cs
+++ hello/hello/Program.cs
@@ -6,11 +6,17 @@ namespace hello
     class Program
 
     {
+       
         static void Main(string[] args)
 
         {
+            
             Console.WriteLine("Hello World!");
             Console.WriteLine("Oh, hi, Mark!");
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine(i);
+            }
             Console.ReadKey();
         }
     }
